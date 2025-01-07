93c46b04854b1ae88af3e9659b84409c77a51d30
Jialan Chen <jchen52@stuy.edu>
2025-01-07 14:51:27 -0500

    added player.c

---
+++ player.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+
+//basic version
+
+//create a lobby
+
+// waiting for a player to exist
+// can send request for a match
+// open the new pipe, and start game
+// communication between the players
+// best of three
+
+//tournament (even num)
+//randomly matched (tell each person)
