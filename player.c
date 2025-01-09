db9aaad31d0a63c7fc402f3b0c516ee88c1d1573
Jialan Chen <jchen52@stuy.edu>
2025-01-09 14:49:34 -0500

    started adding select into program

--- player.c
+++ player.c
@@ -10,35 +10,40 @@
 #define WKP "Lobby"
 #define MAX_PLAYERS 5
 
-//basic version
-struct players {
-  char* ID;
-  int wins;
-}
-
-char** list_players() {
-  char** players;
-  players = (char**)malloc(sizeof(char*)*5); //max 5 players
-  for (int i=0; i<MAX_PLAYERS;i++) {
-    players[i] = (char*)malloc(sizeof(char));
-  }
-  return players;
-}
-
-
 int player_match() {
   int to_server;
   int from_server;
-  from_server = client_handshake( &to_server );
+  int sent = 0;
   char buffer[30];
-  //server sends player the private pipe which they will communicate with other player
-  //for now just the server
-  for (int i=0; i<3; i++) {
-    read(from_server, buffer, sizeof(buffer));
-    printf("Server sent: %s", buffer);
+  
+  from_server = client_handshake( &to_server );
+
+  fd_set read_fds;
+  FD_ZERO(&read_fds);
+  FD_SET(to_server, &read_fds);
+  FD_SET(STDIN_FILENO, &read_fds);
+
+  //stdin player 1 --> server
+  //stdin player 2 --> server
+  
+  
+  if (FD_ISSET(STDIN_FILENO, &read_fds)) {
     fgets(buffer, 30, stdin);
     write(to_server, buffer, sizeof(buffer));
+    printf("Your choice: %s", buffer);
+    sent=1;
   }
+
+  if (FD_ISSET(from_server, &read_fds)) {
+    read(from_server, buffer, sizeof(buffer));
+    //only print when made choice or time up
+    if (sent) {
+      printf("Opponent choice: %s", buffer);
+    }
+  }
+
+
+  
   // char pipe_name[256];
   // sprintf(pipe_name, "%d", getpid());
   // if (mkfifo(pipe_name, 0644)==-1) {
@@ -65,16 +70,12 @@ int player_action() {
   }
 }
 
-// waiting for a player to exist
+// char** list_players() {
+//   char** players;
+//   players = (char**)malloc(sizeof(char*)*5); //max 5 players
+//   for (int i=0; i<MAX_PLAYERS;i++) {
+//     players[i] = (char*)malloc(sizeof(char));
+//   }
+//   return players;
+// }
 
-//player and server connect
-//server creates WKP for all players to go onto-- basically the forking lab
-//player confirm connection to server
-
-// can send request for a match
-// open the new pipe, and start game
-// communication between the players
-// best of three
-
-//tournament (even num)
-//randomly matched (tell each person)
