66d0f2b572ba14a3b06ebc977a0538fbae4d3530
Jialan Chen <jchen52@stuy.edu>
2025-01-13 14:28:49 -0500

    got communciation working between multiple player and server

--- player.c
+++ player.c
@@ -1,22 +1,43 @@
-#include "handshake.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define WKP "lobby"
+#define BUFFER_SIZE 256
 
 int main() {
-  int from_server;
-  int to_server;
-  from_server = client_handshake( &to_server );
+	char pipe_name[10];
+	sprintf(pipe_name, "%d", getpid());
+    mkfifo(pipe_name, 0644);
 
-  char buffer[256];
+    int wkp_fd = open(WKP, O_WRONLY);
+    if (wkp_fd < 0) {
+        perror("Failed to connect to WKP");
+        exit(1);
+    }
+    write(wkp_fd, pipe_name, strlen(pipe_name)+1);
+    close(wkp_fd);
+
+	//write to server
+    int client_fd = open(pipe_name, O_WRONLY);
+    if (client_fd < 0) {
+        perror("Failed to open client pipe");
+        exit(1);
+    }
+
+  char buffer[10];
   printf("Enter...\n");
-  fgets(buffer, 255, stdin);
-  char* action;
-  char* pipe_name; // player id
-  //sscanf(buffer, "%[0-9] %[^\n]", action, pipe_name);
+  fgets(buffer, 10, stdin);
   printf("Attempt to match with player %s", buffer);
-  //printf("Attempting to %s to %s...\n", action, pipe_name);
-  //if (strcmp(action, "match")==0) {
-    //write(to_server, pipe_name, sizeof(pipe_name));
-    write(to_server, buffer, sizeof(buffer));
-    printf("Sent request to player %s", buffer);
-  //}
+  write(client_fd, buffer, sizeof(buffer));
+  printf("Sent request to player %s", buffer);
 
+	
+   close(client_fd);
+   remove(pipe_name);
+   return 0;
 }
--- server.c
+++ server.c
@@ -1,70 +1,87 @@
-#include "handshake.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/select.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
+#define WKP "lobby"
+#define BUFFER_SIZE 256
 #define MAX_CLIENTS 2
-#define BUFFER_SIZE 10
+
+struct list_clients {
+	char pipe_name[BUFFER_SIZE];
+	int client_fd;
+}
 
 int main() {
-    int to_client;
-    int from_client;
+    int wkp_fd, client_fds[MAX_CLIENTS] = {0};
+    int client_count = 0;
+    fd_set read_fds;
     char buffer[BUFFER_SIZE];
 
-    from_client = server_handshake( &to_client );
+    // Create and open the WKP
+    mkfifo(WKP, 0644);
+    wkp_fd = open(WKP, O_RDONLY);
+    if (wkp_fd==-1) {
+        perror("Failed to open WKP");
+        exit(1);
+    }
+    printf("Server started. Waiting for connections...\n");
 
-    fd_set read_fds;
-    int client_fds[MAX_CLIENTS];
-    int listen_socket = from_client;
-    int client_count=0;
+    while (1) {
+        FD_ZERO(&read_fds); //init set
+        FD_SET(wkp_fd, &read_fds); // add WKP to set
+        int max_fd = wkp_fd;
 
-	while (1) {
-    //add WKP to set
-	FD_ZERO(&read_fds);
-	FD_SET(listen_socket, &read_fds);
-    if (select(listen_socket+1, &read_fds, NULL, NULL, NULL)==-1) {
-    	perror("select error");
-    	exit(1);
+		// add clients to set
+        for (int i = 0; i < client_count; i++) {
+            FD_SET(client_fds[i], &read_fds);
+            if (client_fds[i] > max_fd) {
+                max_fd = client_fds[i];
+            }
+        }
+
+        if (select(max_fd + 1, &read_fds, NULL, NULL, NULL)==-1) {
+            perror("select error");
+            exit(1);
+        }
+
+        // check if WKP has a new connection
+        if (FD_ISSET(wkp_fd, &read_fds)) {
+            char client_pipe_name[BUFFER_SIZE];
+            int bytes_read = read(wkp_fd, client_pipe_name, sizeof(client_pipe_name));
+            printf("New client connected: %s\n", client_pipe_name);
+            if (client_count < MAX_CLIENTS) {
+                int client_fd = open(client_pipe_name, O_RDONLY);
+                client_fds[client_count++] = client_fd;
+                
+                } 
+            else {
+                printf("Max clients reached. Cannot accept %s\n", client_pipe_name);
+            }
+        }
+
+        // check if any client FDs have data to read
+        for (int i = 0; i < client_count; i++) {
+            if (FD_ISSET(client_fds[i], &read_fds)) {
+                int bytes_read = read(client_fds[i], buffer, sizeof(buffer) - 1);
+                if (bytes_read > 0) {
+                    buffer[bytes_read] = '\0';
+                    printf("Received from client %d: %s\n", i + 1, buffer);
+                } else if (bytes_read == 0) {
+                    printf("Client %d disconnected\n", i + 1);
+                    close(client_fds[i]);
+                    client_fds[i] = client_fds[--client_count];
+                    i--;
+                }
+            }
+        }
     }
 
-    //add clients to set
-    // for (int i=0; i<MAX_CLIENTS; i++)  {
-      // FD_SET(client_fds[i], &read_fds);
-    // }
-
-    //Server listens for new clients (connection) on WKP
-	char pipe_name[BUFFER_SIZE];
-    for (int i=0;i<MAX_CLIENTS;i++) {
-    	if (FD_ISSET(i, &read_fds)) {
-    		if (i==listen_socket) {
-    			read(listen_socket, &pid, sizeof(pid));
-    			sprintf(pipe_name, "%d", pid);
-    			int client_fds[i] = open(pipe_name, O_RDONLY);
-    			FD_SET(client_fds[i], &read_fds);
-    		}
-    		else {
-    			read(client_fds[i], buffer, sizeof(buffer));
-    			printf("Player choice: %s\n", buffer);
-    			FD_CLR(client_fds[i], &current_sockets);
-    		}
-    	}
-    }
-    // printf("Waiting for connections...\n");
-    // int pid;
-    // if (FD_ISSET(listen_socket, &read_fds)) {
-      // char pipe_name[BUFFER_SIZE];
-      // read(listen_socket, &pid, sizeof(pid));
-      // //printf("Player %d connected to lobby...\n", pid);
-      // sprintf(pipe_name, "%d", pid);
-  	// //add to fd to listen for
-  	// client_fds[client_count] = open(pipe_name, O_RDONLY); //nonblock?
-  	// FD_SET(client_fds[client_count], &read_fds);
-    // client_count++;
-    // }
-
-    // for (int i=0; i<MAX_CLIENTS;i++) {
-   	// if (FD_ISSET(client_fds[i], &read_fds)) {
-   		// read(client_fds[i], buffer, sizeof(buffer));
-   		// printf("Player choice: %s\n", buffer);
-   		// }
-    // }
-   // }
-   return 0;
+    close(wkp_fd);
+    remove(WKP);
+    return 0;
 }
